12130633ada8381b5615b8f42154e3aed3fb72c0
Pawan <kumarpawanm8085@gmail.com>
2023-03-24 22:57:38 +0530

    did some changes

--- Ecommerce/src/main/java/com/ecommerce/controller/HomeController.java
+++ Ecommerce/src/main/java/com/ecommerce/controller/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 	
+	@GetMapping
 	public String welcome() {
 		
 		return "Welcome to this application!";
